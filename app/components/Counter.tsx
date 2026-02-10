4bc94f0ef9634c5c27618aa5afaebc0f172b327e
sneema-design <sneema@gammastack.com>
2026-02-10 18:21:00 +0530

    learned-use-client

--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -2,14 +2,16 @@
 
 import { useState } from "react"
 
-export default function counter() {
+export default function Counter() {
     const  [count,setCount] = useState(0)
     return(
         <div>
 
             <button onClick={()=>(setCount(count+1))}>
+                <br/>
             +
         </button>
+        <br/>
         {count}
         </div>
         
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import Counter from "./components/Counter";
+
 export default function Home() {
   console.log("this is home page");
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       welcome to home page
+      <Counter/>
     </div>
   );
 }
